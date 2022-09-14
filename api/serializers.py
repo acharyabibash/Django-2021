00483a7ed5056350868005ef4dba5a79a143779e
acharyabibash <acharabibash@outlook.com>
2022-09-14 16:04:06 +0545

    Python Django 2021: Section12 : module 2 Dennis Ivy

--- api/serializers.py
+++ api/serializers.py
@@ -37,4 +37,10 @@
 
 
 # after commenting
+from rest_framework import serializers
+from projects.models import Project
 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields= '__all__'
