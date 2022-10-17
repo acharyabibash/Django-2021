966d13bdfb0aaec4d106116a9814a9dc5aa638dd
acharyabibash <acharabibash@outlook.com>
2022-10-17 16:44:02 +0545

    Section 12: Module 7
    Django 2021 by Dennis Ivy

--- frontend/main.js
+++ frontend/main.js
@@ -108,12 +108,27 @@ let getProjects = () => {
 }
 
 let buildProjects = (projects) => {
-    let projectsWrapper = document.getElementById('projects-wrapper')
+    let projectsWrapper = document.getElementById('projects--wrapper')
 
     for(let i=0; projects.length > i ; i++){
         let project = projects[i]
         
+        let projectCard = `
+                <div class="project--card">
+                    <img src="http://127.0.0.1:8000${project.featured_image}" />
+                    <div>
+                        <div class="card--header">
+                        <h3>${project.title}</h3>
+                        <strong class="vote--option" >&#43;</strong>
+                        <strong class="vote--option" >&#8722;</strong>
+                        </div>
+                        <i>${project.vote_ratio}% Positive feedback </i>
+                        <p>${project.description.substring(0,150)}</p>
+                    </div>
+                </div>
         
+        `
+        projectsWrapper.innerHTML += projectCard
     }
 }
 
